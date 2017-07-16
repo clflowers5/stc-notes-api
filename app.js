e0e62ba2a15cde8f77cf719c3dca3084e60d181c
Chris Flowers <clflowers5@gmail.com>
2017-07-15 18:22:56 -0600

    add cors headers for responses

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
+// CORS headers
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
